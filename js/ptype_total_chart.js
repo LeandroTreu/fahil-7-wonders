d8d3262e6085704712cf5e0af4d91c4c816f5521
ltreu <leandro.treu@gmail.com>
2023-12-12 01:22:09 +0100

    new stats

---
+++ js/ptype_total_chart.js
@@ -0,0 +1,54 @@
+(async function() {
+
+  Chart.defaults.font.size = 18;
+  Chart.defaults.borderColor = '#9E9E9E';
+  Chart.defaults.color = '#FFF';
+  Chart.defaults.plugins.legend.display = false;
+
+    const data = {
+      labels: ['Wonder', 'Money', 'War', 'Blue', 'Yellow', 'Green', 'Purple', 'Cities', 'Leaders', 'Sea War', 'Sea Cards'],
+      datasets: [{
+        axis: 'y',
+        label: 'Total Points',
+        data: [829, 504, 603, 1405, 440, 1087, 694, 46, 257, 119, 98],
+        fill: false,
+        backgroundColor: [
+          '#b28448',
+          '#c4813b',
+          '#a93e36',
+          '#42739c',
+          '#c07e32',
+          '#4f6a41',
+          '#5d567f',
+          '#5c4c3f',
+          '#a9a196',
+          '#445778',
+          '#5882aa'
+        ],
+        borderWidth: 0,
+        minBarLength: 8,
+        // barThickness: 30,
+        categoryPercentage: 0.8
+      }]
+    };
+
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          x: {
+            grace: '0%'
+          }
+        },
+        indexAxis: 'y',
+        aspectRatio: 1.0
+      }
+    };
+  
+    new Chart(
+      document.getElementById('ptype_total_chart'),
+      config
+    );
+
+  })();
---
+++ js/winner_max_ptype_chart.js
@@ -0,0 +1,55 @@
+(async function() {
+
+  Chart.defaults.font.size = 18;
+  Chart.defaults.borderColor = '#9E9E9E';
+  Chart.defaults.color = '#FFF';
+  Chart.defaults.plugins.legend.display = false;
+
+    const data = {
+      labels: ['Wonder', 'Money', 'War', 'Blue', 'Yellow', 'Green', 'Purple', 'Cities', 'Leaders', 'Sea War', 'Sea Cards'],
+      datasets: [{
+        axis: 'y',
+        label: 'Total Points',
+        //     w  m  w  b  y  g  p  c  l  s  s
+        data: [2, 0, 1, 4, 0, 6, 4, 0, 0, 0, 0],
+        fill: false,
+        backgroundColor: [
+          '#b28448',
+          '#c4813b',
+          '#a93e36',
+          '#42739c',
+          '#c07e32',
+          '#4f6a41',
+          '#5d567f',
+          '#5c4c3f',
+          '#a9a196',
+          '#445778',
+          '#5882aa'
+        ],
+        borderWidth: 0,
+        minBarLength: 8,
+        // barThickness: 30,
+        categoryPercentage: 0.8
+      }]
+    };
+
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          x: {
+            grace: '0%'
+          }
+        },
+        indexAxis: 'y',
+        aspectRatio: 1.0
+      }
+    };
+  
+    new Chart(
+      document.getElementById('winner_max_ptype_chart'),
+      config
+    );
+
+  })();
